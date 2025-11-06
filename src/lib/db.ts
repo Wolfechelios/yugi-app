a0a31bfbd0f2f3752e9bbf2fc153446ebb0bc698
Wolfechelios <132318325+Wolfechelios@users.noreply.github.com>
2025-11-06 03:53:26 -0800

    Replace Supabase client with Prisma client

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,2 @@
-import { createClient } from "@supabase/supabase-js";
-
-export const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL!,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
-);
+import { PrismaClient } from "@prisma/client";
+export const db = new PrismaClient();
